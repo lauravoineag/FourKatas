c435dad729abb2c80eb83f051d88ead655601578
Laura Voineag <laura_voineag@waters.com>
2022-12-09 13:50:28 +0000

    Add code for ab value to return false and pass failing test

--- PalindromesKata/Palindromes.cs
+++ PalindromesKata/Palindromes.cs
@@ -4,6 +4,10 @@ public class Palindromes
 {
     public bool Execute( string value)
     {
+        if (value == "ab")
+        {
+            return false;
+        }
         return true;
     }
 }
