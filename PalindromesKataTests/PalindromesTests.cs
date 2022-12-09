ab60dbaca5b9cba0a033c57ab9ef2a0103278047
Laura Voineag <laura_voineag@waters.com>
2022-12-09 13:47:28 +0000

    Add failing test ab value to return false

--- PalindromesKataTests/PalindromesTests.cs
+++ PalindromesKataTests/PalindromesTests.cs
@@ -31,4 +31,15 @@ public class PalindromesTests
         //assert 
         Assert.That(actual,Is.EqualTo(true));
     }
+    
+    [Test]
+    public void when_I_pass_ab_I_will_get_false()
+    {
+        //arrange
+        var palindromes = new Palindromes();
+        //act 
+        var actual = palindromes.Execute("ab");
+        //assert 
+        Assert.That(actual,Is.EqualTo(false));
+    }
 }
