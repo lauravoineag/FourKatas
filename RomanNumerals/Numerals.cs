25278dd96af79cfc80f04ac44dbf69fef728c905
Laura <lauravoineag87@gmail.com>
2022-12-10 18:04:01 +0000

    Add code for Roman Numeral IV to pass failing test

--- RomanNumerals/Numerals.cs
+++ RomanNumerals/Numerals.cs
@@ -4,6 +4,10 @@ public class Numerals
 {
     public string Convert(int number)
     {
+        if (number == 4)
+        {
+            return "IV";
+        }
         return new string('I', number);
     }
 }
