4d8ac4fceb02775037b9114eb62820fe1a976e1d
Laura <lauravoineag87@gmail.com>
2022-12-10 16:32:58 +0000

    Add failing tests to check whitespace and punctuation

--- PalindromesKata/Palindromes.cs
+++ PalindromesKata/Palindromes.cs
@@ -4,9 +4,11 @@ public class Palindromes
 {
     public bool IsPalindrome(string input)
     {
-        var reversedChar = input.Reverse().ToArray(); //string is IEnumerable of char
-        var reversed = new String(reversedChar); //converting Ienumerable of char into string.
-        if  (string.Equals(input,reversed,StringComparison.CurrentCultureIgnoreCase))
+        //strip whitespace and punctuation
+        
+        var reversedChar = input.Reverse().ToArray(); //string is IEnumerable of char // String constructor needs an Array
+        var reversed = new String(reversedChar); //converting IEnumerable of char into string.
+        if  (string.Equals(input,reversed,StringComparison.CurrentCultureIgnoreCase))//check case sensitive
         {
             return true;
         }
--- PalindromesKataTests/PalindromesTests.cs
+++ PalindromesKataTests/PalindromesTests.cs
@@ -44,12 +44,23 @@ public class PalindromesTests
     }
     
     [Test]
-    public void when_I_pass_a_mixed_palindrome_I_will_get_true()
+    public void when_I_pass_mixed_case_palindrome_I_will_get_true()
     {
         //arrange
         var palindromes = new Palindromes();
         //act 
-        var actual = palindromes.IsPalindrome("aA");
+        var actual = palindromes.IsPalindrome("Able was I ere I saw Elba");
+        //assert 
+        Assert.That(actual,Is.EqualTo(true));
+    }
+    
+    [Test]
+    public void when_I_pass_palindrome_containing_whitespace_and_punctuation_I_will_get_true()
+    {
+        //arrange
+        var palindromes = new Palindromes();
+        //act 
+        var actual = palindromes.IsPalindrome("A man, a plan, a canal, Panama!");
         //assert 
         Assert.That(actual,Is.EqualTo(true));
     }
