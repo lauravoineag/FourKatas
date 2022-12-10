54a9da4d8937f1cb76692483a5e8eee4fdb40875
Laura <lauravoineag87@gmail.com>
2022-12-10 17:14:17 +0000

    Add Palindrome check console app

--- PalindromesKata/Program.cs
+++ PalindromesKata/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using PalindromesKata;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Type a string and I'll tell you if it's a palindrome");
+
+var input = Console.ReadLine();
+var palindrome = new Palindromes();
+var result = palindrome.IsPalindrome(input);
+
+var answer = result ? "Yay it's a palindrome!" : "Tough luck it's not a palindrome :(";
+Console.WriteLine(answer);
