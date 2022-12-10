fca3ee9c84a59ccfd69e9abd6c15ea7d1f7a62f6
Laura <lauravoineag87@gmail.com>
2022-12-10 16:54:12 +0000

    Ignore punctuation and whitespace when testing for palindome

--- PalindromesKata/Palindromes.cs
+++ PalindromesKata/Palindromes.cs
@@ -4,17 +4,15 @@ public class Palindromes
 {
     public bool IsPalindrome(string input)
     {
-        //strip whitespace and punctuation
-        
-        var reversedChar = input.Reverse().ToArray(); //string is IEnumerable of char // String constructor needs an Array
+        //filtering punctuation characters out of the string.
+        var newInput = new string(input.Where(c => !char.IsPunctuation(c)&& !char.IsWhiteSpace(c)).ToArray());
+        var reversedChar = newInput.Reverse().ToArray(); //string is IEnumerable of char // String constructor needs an Array
         var reversed = new String(reversedChar); //converting IEnumerable of char into string.
-        if  (string.Equals(input,reversed,StringComparison.CurrentCultureIgnoreCase))//check case sensitive
+        if (string.Equals(newInput, reversed, StringComparison.CurrentCultureIgnoreCase)) //check case sensitive
         {
             return true;
         }
+
         return false;
     }
 }
-
-    
-
